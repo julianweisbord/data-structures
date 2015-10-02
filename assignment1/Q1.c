a9aae1f3046911b923ea4fbaa59908b9920b4663
julianweisbord <julianweisbord@gmail.com>
2015-10-01 22:19:55 -0700

    Generated list of unique ids.
    Signed-off-by: julianweisbord <julianweisbord@gmail.com>

--- assignment1/Q1.c
+++ assignment1/Q1.c
@@ -15,42 +15,27 @@ struct student{
 
 struct student* allocate(){
      /*Allocate memory for ten students*/
-     struct student *studentArray = malloc(10 * sizeof(struct student));
+     struct student *students = malloc(10 * sizeof(struct student));
      /*return the pointer*/
-     return studentArray;
+     return students;
 }
 
 void generate(struct student* students){
      /*Generate random ID and scores for 10 students, ID being between 1 and 10, scores between 0 and 100*/
-		 int i,j, possibleID;
-		 time_t timeVal;
-		 srand((unsigned) time(&timeVal));
-
-		//  ids must be unique, why do some become 0?
-		int check = 0;
-		int one =1;
-		for(i=0; i< 10; ++i){
-			while(one >0){
-				possibleID = (rand() % 10);
-				for(j=0; j< 10; ++j){
-				 	if(students[j].id ==possibleID){
-						check =1;
-						one =0; //to break out of while
-						break; //break out of for
-				 	}
-			 	}
-				if(check ==0){
-					break;
-				}
-
-			} //while
-			one = 1;
-			if(check ==0){
-			 	students[i].id = possibleID;
-			}
-			 	students[i].score = (rand() % 100);
+		time_t timeVal;
+		srand((unsigned) time(&timeVal));
+		int i,j;
+		for(i = 0; i < 10; ++i){
+		  students[i].id= (rand() % 10);
+			students[i].score = (rand() % 100);
+		  for(j =0; j< i; ++j){
+		    if(students[i].id == students[j].id){
+		      students[i].id= (rand() % 10);
+		      j = -1;
+		    }
+		  }
 		}
-}
+	}
 
 void output(struct student* students){
 		int i;
