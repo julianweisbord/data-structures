8a19b9066c23d09152d84016286207ce302b9436
julianweisbord <julianweisbord@gmail.com>
2015-10-16 20:26:02 -0700

    Everything working.
    Signed-off-by: julianweisbord <julianweisbord@gmail.com>

--- assignment2/calc.c
+++ assignment2/calc.c
@@ -4,6 +4,8 @@
 #include <math.h>
 #include "dynamicArray.h"
 
+// example run is: ./calc 5 4 ^
+// This would be 4^5 power
 
 /* param: s the string
    param: num a pointer to double
@@ -21,10 +23,9 @@ int isNumber(char *s, double *num)
 		*num = 0;
 		return 1;
 	}
-	else
-	{
+	else{
 		returnNum = strtod(s, &end);
-		/* If there's anythin in end, it's bad */
+		/* If there's anything in end, it's bad */
 		if((returnNum != 0.0) && (strcmp(end, "") == 0))
 		{
 			*num = returnNum;
@@ -43,6 +44,10 @@ void add (struct DynArr *stack)
 {
 	/* FIXME: You will write this function */
 	// main checks if there are less than 2 args
+	if (sizeDynArr(stack) <2){
+		printf("Not enough args!\n");
+		exit(0);
+	}
 	double i =topDynArr(stack);
 	popDynArr(stack);
 	double j =topDynArr(stack);
@@ -134,6 +139,25 @@ void logar(struct DynArr *stack){
 	pushDynArr(stack, log10(i));
 }
 
+void incorrect(struct DynArr *stack, char* arg){
+	int size;
+
+	if (isEmptyDynArr(stack) == 0)
+		size = sizeDynArr(stack);
+	else
+		size = 0;
+
+
+	printf("Invalid string: ");
+	for (int i = 0; i < size; ++i){
+		printf("%f ", getDynArr(stack, i));
+	}
+	printf("%s\n",arg);
+	deleteDynArr(stack);
+	exit(0);
+
+}
+
 
 
 double calculate(int numInputTokens, char **inputString)
@@ -158,55 +182,134 @@ double calculate(int numInputTokens, char **inputString)
 		//     (1b - I) If s is not a number, produce an error.
 		//     (1b - II) If s is a number, push it onto the stack
 
-		if(strcmp(s, "+") == 0)
-			add(stack);
-		else if(strcmp(s,"-") == 0)
-			subtract(stack);
-		else if(strcmp(s, "/") == 0)
-			divide(stack);
-		else if(strcmp(s, "x") == 0)
+		if(strcmp(s, "+") == 0){
+			if(isEmptyDynArr(stack) ==0 && sizeDynArr(stack) >=2){
+				add(stack);
+			}
+			else{
+				incorrect(stack,"+");
+			}
+		}
+		else if(strcmp(s,"-") == 0){
+			if(isEmptyDynArr(stack) ==0 && sizeDynArr(stack) >=2)
+				subtract(stack);
+			else{
+				incorrect(stack,"-");
+			}
+		}
+		else if(strcmp(s, "/") == 0){
+
+
+			if(isEmptyDynArr(stack) ==0 && sizeDynArr(stack) >=2)
+				divide(stack);
+			else{
+				incorrect(stack,"/");
+			}
+		}
+		else if(strcmp(s, "x") == 0){
 			/* FIXME: replace printf with your own function */
-			multiply(stack);
-		else if(strcmp(s, "^") == 0)
+			if(isEmptyDynArr(stack) ==0 && sizeDynArr(stack) >=2)
+				multiply(stack);
+			else{
+				incorrect(stack,"x");
+			}
+		}
+
+		else if(strcmp(s, "^") == 0){
 			/* FIXME: replace printf with your own function */
-			power(stack);
-		else if(strcmp(s, "^2") == 0)
+			if(isEmptyDynArr(stack) ==0 && sizeDynArr(stack) >=2)
+				power(stack);
+			else{
+				incorrect(stack,"^");
+			}
+		}
+		else if(strcmp(s, "^2") == 0){
 			/* FIXME: replace printf with your own function */
 			// printf("Squaring\n");
-			square(stack);
-		else if(strcmp(s, "^3") == 0)
+
+			if(isEmptyDynArr(stack) ==0 && sizeDynArr(stack) >=1)
+				square(stack);
+			else{
+				incorrect(stack,"^2");
+			}
+		}
+		else if(strcmp(s, "^3") == 0){
 			/* FIXME: replace printf with your own function */
 			// printf("Cubing\n");
-			cube(stack);
-		else if(strcmp(s, "abs") == 0)
+			if(isEmptyDynArr(stack) ==0 && sizeDynArr(stack) >=1)
+				cube(stack);
+			else{
+				incorrect(stack,"^3");
+			}
+		}
+		else if(strcmp(s, "abs") == 0){
 			/* FIXME: replace printf with your own function */
 			// printf("Absolute value\n");
-			absolute(stack);
-		else if(strcmp(s, "sqrt") == 0)
+
+			if(isEmptyDynArr(stack) ==0 && sizeDynArr(stack) >=1)
+				absolute(stack);
+			else{
+				incorrect(stack,"abs");
+			}
+		}
+		else if(strcmp(s, "sqrt") == 0){
 			/* FIXME: replace printf with your own function */
 			// printf("Square root\n");
-			squareroot(stack);
-		else if(strcmp(s, "exp") == 0)
+			if(isEmptyDynArr(stack) ==0 && sizeDynArr(stack) >=1)
+				squareroot(stack);
+			else{
+				incorrect(stack,"sqrt");
+			}
+
+		}
+		else if(strcmp(s, "exp") == 0){
 			/* FIXME: replace printf with your own function */
 			// printf("Exponential\n");
-			exponent(stack);
-		else if(strcmp(s, "ln") == 0)
+
+			if(isEmptyDynArr(stack) ==0 && sizeDynArr(stack) >=1)
+				exponent(stack);
+			else{
+				incorrect(stack,"exp");
+			}
+
+		}
+		else if(strcmp(s, "ln") == 0){
 			/* FIXME: replace printf with your own function */
-			ln(stack);
-		else if(strcmp(s, "log") == 0)
+			if(isEmptyDynArr(stack) ==0 && sizeDynArr(stack) >=1)
+				ln(stack);
+			else{
+				incorrect(stack,"ln");
+			}
+		}
+		else if(strcmp(s, "log") == 0){
 			/* FIXME: replace printf with your own function */
-			logar(stack);
-		else
-		{
+
+			if(isEmptyDynArr(stack) ==0 && sizeDynArr(stack) >=1)
+				logar(stack);
+			else{
+				incorrect(stack,"log");
+			}
+		}
+		else{
 			// FIXME: You need to develop the code here (when s is not an operator)
 			// Remember to deal with special values ("pi" and "e")
 			if(strcmp(s, "e") ==0){
-				s = "2.7182818";
+				pushDynArr(stack, 2.7182818);
 
 			}
-			if(strcmp(s, "pi") ==0){
-				s = "3.14159265";
+			else if(strcmp(s, "pi") ==0){
+				pushDynArr(stack, 3.14159265);
 
+			}
+			else{
+				double num;
+				if(isNumber(s, &num) == 1){
+					pushDynArr(stack, num);
+				}
+				else{
+					printf("INVALID NUMBER!: %s\n", s);
+					exit(0);
+				}
 			}
 
 		}
@@ -216,7 +319,20 @@ double calculate(int numInputTokens, char **inputString)
 	 * (1) Check if everything looks OK and produce an error if needed.
 	 * (2) Store the final value in result and print it out.
 	 */
+	result = topDynArr(stack);
+	popDynArr(stack);
+	int stack_size = sizeDynArr(stack);
+	if(stack_size !=0){
 
+
+		for(i=0;i<stack_size;++i){
+			printf("The value left is: %1f", getDynArr(stack, i));
+		}
+		deleteDynArr(stack);
+		exit(0);
+	}
+	printf("RESULT: %1f\n", result);
+	deleteDynArr(stack); //deallocate
 	return result;
 }
 
@@ -224,7 +340,7 @@ int main(int argc , char** argv)
 {
 	// assume each argument is contained in the argv array
 	// argc-1 determines the number of operands + operators
-	if (argc <= 2){
+	if (argc <= 1){
 		printf("Only one or less argument\n");
 		return 0;
 	}
